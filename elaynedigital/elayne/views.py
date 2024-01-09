e1caf88ed8fc8d99ad591a7beacca89f96edcf54
gayathry <gayathripalliyath@gmail.com>
2024-01-09 08:19:24 +0530

    message

--- elaynedigital/elayne/views.py
+++ elaynedigital/elayne/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from. forms import *
 from. models import *
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    enq=Contact_us.objects.all()
+    test_model=Testimonials.objects.all()
+    form_obj=Contact_form()
+    if request.method == 'POST':
+        if 'save' in request.POST:
+            form=Contact_form(request.POST)
+            form.save()
+    context=[]
+    context={
+        "form_obj":form_obj,
+        "enq":enq,
+        "test_model":test_model
+    }        
+    return render(request,'index.html',context)
